f51f3267e537b8ccc52ae7de98a5cb100537bbf0
Victor Marcilio A. S. Peixoto <vmp@uvic.ca>
2021-02-19 09:56:19 -0300

    Create uri3146.cpp

---
+++ uri3146.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    double R;
+    cin >> R;
+    cout << setprecision(2) << fixed << R * 2 * 3.14 << '\n';
+}
