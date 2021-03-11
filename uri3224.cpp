c1e83915f8abd7a32a7e3ea3c157af5b3cf335b3
Victor Marcilio A. S. Peixoto <vmp@uvic.ca>
2021-03-11 13:24:28 -0300

    Create uri3224.cpp

---
+++ uri3224.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s1, s2;
+    cin >> s1 >> s2;
+    cout << (s1.size() >= s2.size() ? "go\n" : "no\n");
+}
