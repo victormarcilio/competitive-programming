91d76792fde4eb14eb2b25ab178b15fb030a89fd
Victor Marcilio Peixoto <79210944+victormarcilio@users.noreply.github.com>
2021-07-10 23:54:24 -0300

    Create codesignal-containsCloseNums.cpp

---
+++ codesignal-containsCloseNums.cpp
@@ -0,0 +1,14 @@
+//https://app.codesignal.com/interview-practice/task/njfXsvjRthFKmMwLC
+bool containsCloseNums(vector<int> nums, int k) {
+    unordered_map<int, int> m;
+    for(auto i = 0; i < nums.size(); i++)
+    {
+        if(m.find(nums[i]) != m.end())
+        {
+            if(i - m[nums[i]] <= k)
+                return true;            
+        }   
+        m[nums[i]] = i;
+    }
+    return false;
+}
